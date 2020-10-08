39bbc0f5a367b12e35b062491837df390f4417f9
Ivorystone <45602845+Ivorystone@users.noreply.github.com>
2020-10-07 19:41:36 -0500

    Create User.java

---
+++ src/Model/User.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class User {
+
+    private String username;
+
+    //Constructor
+    public User() {}
+
+    //Getter
+    public String getUsername() { return username; }
+
+    //Setter
+    public void setUsername(String username) { this.username = username; }
+
+}
