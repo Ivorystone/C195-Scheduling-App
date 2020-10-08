68b54cc611ae80792d32bf205080ac308fa42ca0
Ivorystone <45602845+Ivorystone@users.noreply.github.com>
2020-10-07 19:43:32 -0500

    Create DBQuery.java

---
+++ src/Utility/DBQuery.java
@@ -0,0 +1,21 @@
+package Utility;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DBQuery {
+
+    private static Statement statement; //statement reference
+
+    //Create statement object
+    public static void setStatement(Connection conn) throws SQLException {
+        statement = conn.createStatement();
+    }
+
+    //Return statement object
+    public static Statement getStatement(){
+        return statement;
+    }
+}
